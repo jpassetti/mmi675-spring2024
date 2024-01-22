2f22934597ddcdb72d9afc69beac3e22f5143d94
Jeff Passetti <jpassetti@gmail.com>
2024-01-22 18:48:25 -0500

    added starbucks

---
+++ 02/starbucks/index.php
@@ -0,0 +1,8 @@
+<?php include "partials/header.php"; ?>
+    
+<main>
+    <h2>Our coffee</h2>
+    <p>Main content goes here.</p>
+</main>
+
+<?php include "partials/footer.php"; ?>
---
+++ 02/starbucks/partials/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Starbucks</title>
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+    <header>
+        <h1>Starbucks</h1>
+        <p>Logo goes here.</p>
+        <nav>
+            <ul>
+                <li>
+                    <a href="index.php">Home</a>
+                </li>
+                <li>
+                    <a href="products.php">Products</a>
+                </li>
+                <li>
+                    <a href="about.php">About</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
